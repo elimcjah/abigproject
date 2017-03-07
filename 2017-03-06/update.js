7edb13fe108458155a6af3fe4a2dc94d1fc87fbe
Elijah McClendon <Elijah@fungry.com>
2017-03-07 13:35:11 -0700

    update

--- 2017-03-06/update.js
+++ 2017-03-06/update.js
@@ -8,10 +8,10 @@
  */
 function updateFileInScheduler() {
     let fs = require('fs');
-    fs.writeFileSync('2017-03-06/student/elimcjah.txt', (new Date()).getTime());
+    fs.writeFileSync('student/elimcjah.txt', (new Date()).getTime());
 
     let d1 = (new Date(
-        parseInt(fs.readFileSync('2017-03-06/student/elimcjah.txt', 'UTF8'))));
+        parseInt(fs.readFileSync('student/elimcjah.txt', 'UTF8'))));
     let d2 = (new Date());
     console.assert(Math.abs(d1.getTime() - d2.getTime()) < 5000);
 }
@@ -21,7 +21,7 @@ function updateFileInScheduler() {
  */
 function kickOffAddCommitPushScript() {
     let cp = require('child_process');
-    cp.execFileSync('2017-03-06/push.sh');
+    cp.execFileSync('push.sh');
 }
 
 
