595f6559f530ba53b11b5e1c56bd6f9ddf74a84c
Elijah McClendon <Elijah@fungry.com>
2017-04-03 18:17:59 -0600

    All Homework is now complete

--- modules/delete.js
+++ modules/delete.js
@@ -12,36 +12,36 @@
 // 'horse' ('nay'), 'lion' ('meow', 'roar'), 'dragon'
 // ('roar'), cat ('meow'), dog ('woof', 'howl'), wolf ('howl')
 
-function whatSoundFrom(animalType){
-
-    switch (animalType){
-
-        default:
-            console.log('please choose: horse, lion, dragon, cat, dog, or wolf');
-            break;
-        case 'horse':
-            console.log('nay');
-            break;
-        case 'lion':
-            console.log('meow');
-            console.log('roar');
-            break;
-        case 'dragon':
-            console.log('roar');
-            break;
-        case 'cat':
-            console.log('meow');
-            break;
-        case 'dog':
-            console.log('woof');
-            console.log('howl');
-            break;
-        case 'wolf':
-            console.log('howl');
-            break;
-    }
-}
-
-whatSoundFrom('wolf');
+// function whatSoundFrom(animalType){
+//
+//     switch (animalType){
+//
+//         default:
+//             console.log('please choose: horse, lion, dragon, cat, dog, or wolf');
+//             break;
+//         case 'horse':
+//             console.log('nay');
+//             break;
+//         case 'lion':
+//             console.log('meow');
+//             console.log('roar');
+//             break;
+//         case 'dragon':
+//             console.log('roar');
+//             break;
+//         case 'cat':
+//             console.log('meow');
+//             break;
+//         case 'dog':
+//             console.log('woof');
+//             console.log('howl');
+//             break;
+//         case 'wolf':
+//             console.log('howl');
+//             break;
+//     }
+// }
+//
+// whatSoundFrom('wolf');
 
 
